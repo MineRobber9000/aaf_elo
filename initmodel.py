1d50d9a8dc96b6d47584ea49c49ddee8ad16e95a
khuxkm fbexl <khuxkm@ttm.sh>
2019-02-16 17:22:13 -0500

    Display results of games and correctly score Elo

--- initmodel.py
+++ initmodel.py
@@ -4,7 +4,11 @@ league = aaf.League("Legends Apollos Iron Express Stallions Hotshots Commanders 
 with open("aaf_games.csv") as f:
 	games = list(csv.reader(f))
 for game in games:
-	league.match(league.team[game[0]],league.team[game[1]],1)
+	if game[2]=="0.5":
+		print("{} and {} tie".format(*game))
+	else:
+		print("{} def. {}".format(*([game[0],game[1]] if game[2]=="1" else [game[1],game[0]])))
+	league.match(league.team[game[0]],league.team[game[1]],float(game[2]))
 for team in league.team:
 	print(team,league.team[team].score)
 rows = [[team,league.team[team].score] for team in league.team.keys()]
